c804f92b3247a278cfbb301674a89d61753a8de2
Umair Shahid <mrumairshahid909981@gmail.com>
2026-07-06 11:40:57 +0500

    Revert "inventory system and others"

--- dictionaryand sets/countitems.py
+++
@@ -1,8 +0,0 @@
-fruits = {
-    "apple": 5,
-    "banana": 8,
-    "orange": 3
-}
-print(len(fruits))
-print(fruits.keys())
-print(fruits.values())
--- dictionaryand sets/useupdate.py
+++
@@ -1,7 +0,0 @@
-student = {
-    "name": "Ali",
-    "age": 20
-}
-#use update to add value or update data
-student.update({"marks": 90})
-print(student)
